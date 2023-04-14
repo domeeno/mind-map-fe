a9283bc587d11821fa4458ab9b3d6e78bcfd955c
domeeno <dominicflocea.dev@gmail.com>
2023-04-14 17:37:19 +0300

    add logout

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
   return (
     <div>
       <h1>Mind-Map</h1>
+      <button onClick={() => props.keycloak.logout()}>Logout</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ keycloak
     const root = ReactDOM.createRoot(document.getElementById("root"));
     root.render(
       <React.StrictMode>
-        <App />
+        <App keycloak={keycloak} />
       </React.StrictMode>
     );
 
